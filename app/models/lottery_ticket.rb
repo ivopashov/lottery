9a5add7f6b8c614148eab0814821cd2f21418099
Ivo Pashov <ivo.pashoff@gmail.com>
2019-05-01 11:06:21 +0300

    Add number validation on lottery ticket
    We need to check that the numbers assigned to a
    ticket are exaclty 6 and they are between 1 and 49 inclusively.
    We use custom model validations for the purpose.
    
    A db constraint on that one would be best as we want data integrity
    as close to the db as it gets. Good candidate for improvement.
    
    I intentionally don't put such validation on the lottery draw model as
    it generates numbers by itself (no user input) and don't feel like the
    validation is needed. Probably another good candidate for improvement
    is to have a module generate the six numbers instead of being method on the model
    and extensively test that.

--- app/models/lottery_ticket.rb
+++ app/models/lottery_ticket.rb
@@ -1,6 +1,20 @@
 class LotteryTicket < ApplicationRecord
   belongs_to :lottery_draw, optional: true
 
+  validate :validate_numbers_in_range, :validate_numbers_are_six
+
+  def validate_numbers_in_range
+    if numbers.any? { |number| !number.in? (1..49) }
+      errors.add(:numbers, 'Numbers should be between 1 and 49')
+    end
+  end
+
+  def validate_numbers_are_six
+    if numbers.size != 6
+      errors.add(:numbers, 'Numbers should be exactly 6')
+    end
+  end
+
   class << self
     def not_played_yet
       where lottery_draw: nil
